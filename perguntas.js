ac717e38ad3b0aff4aeee5e0375e6c899fe317d3
Ayakay <120978042+Ayaraky@users.noreply.github.com>
2024-10-28 10:37:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Programação', 
+    'O que é o Python?', 
+    'O JavaScrpit é uma linguagem de programação'
+)
+
+criaCartao (
+    'Geografia', 
+    'Qual a capitão da França', 
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programação', 
+    'O que é uma função?', 
+    'Uma função é um bloco de codigo que executa alguma tarefa'
+)
+
+criaCartao (
+    'Idioma',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi (Rai)'
+)
